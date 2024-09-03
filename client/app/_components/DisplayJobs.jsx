b6d0ee6db55fcb015f0f57f641febfa0707a42a5
Imtiaz Uddin <iuashrafi@gmail.com>
2024-09-03 19:45:46 +0530

    temp data

--- client/app/_components/DisplayJobs.jsx
+++ client/app/_components/DisplayJobs.jsx
@@ -6,7 +6,7 @@ import JobCard from "./JobCard";
 const DisplayJobs = ({ jobs }) => {
   return (
     <section className="p-4 md:px-0 py-16 container bg-green-30 mx-auto grid grid-cols-12 gap-6">
-      {jobs.map((job) => (
+      {jobsData.map((job) => (
         <JobCard key={job._id} job={job} />
       ))}
     </section>
